9e055c0b4e62072cab3f8e1c7cd98d2c88a4f6f6
KC <kamnmclark@gmail.com>
2018-08-12 09:51:39 -0400

    HTML layout and node functionality

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var tom = {
+    "name":"Tom",
+    "photo":"https://imgc.allpostersimages.com/img/print/posters/tom-selleck_a-G-5245117-13198926.jpg",
+    "scores":[
+        5,
+        5,
+        5,
+        4,
+        4,
+        4,
+        3,
+        3,
+        3,
+        2
+      ]
+  };
+
+var ahmed = {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+var friendsData = [tom];
+
+module.exports = friendsData;
